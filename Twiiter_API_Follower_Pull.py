5d581eaa064346c947df5c0755a5c1bdb209a5ca
wward9 <wward949@gmail.com>
2017-11-15 21:54:04 -0500

    Added account vector loop
    Added the ability to loop through a vector of accounts and automatically assign the relevant page to each account.

--- Twiiter_API_Follower_Pull.py
+++ Twiiter_API_Follower_Pull.py
@@ -6,14 +6,16 @@ Created on Sun Nov 12 14:32:27 2017
 @author: william
 
 to do: 
+    -wrap into a function
     -Create vector of revevant twiiter pages
-    -Build loop to pull all followers
     -Automatically assign user to personality categories based on pages
+    
     
 """
 
 import tweepy
 import time
+import pandas as pd
 
 # Variables that contains the user credentials to access Twitter API 
 ACCESS_TOKEN = '929809459576295424-bqkggk2eCOGwG41FjdJJBrDIcmbuWWY'
@@ -22,7 +24,7 @@ CONSUMER_KEY = '2YZ3l8upEuXxoTmqZGFXNCafG'
 CONSUMER_SECRET = '8AhZnoL9bM1pcLkWAQJ7GZFdPp27YDRyfKImnaHogP3F1taNGC'
 
 #Test account to pull followers from
-accountvar = 'wward9'
+accountvar = ['wward9', 'LeanneHanson']
 
 #Get autorization to twitter API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -30,18 +32,29 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-#Pull 200 followers per call
-users = tweepy.Cursor(api.followers, screen_name=accountvar, count=200).items()
+user_list_df = pd.DataFrame(columns=['User','Page'])
 
-userlist = []
 
-#Avoid rate limit by waiting 16 minutes every time the limit is reached
-while True:
-    try:
-        user = next(users)
-    except tweepy.TweepError:
-        time.sleep(60*16)
-        user = next(users)
-    except StopIteration:
-        break
-    userlist.append("@" + user.screen_name)
+for i in range(0, len(accountvar)):
+    
+    user_list = []
+
+    #Pull 200 followers per call
+    users = tweepy.Cursor(api.followers, screen_name=accountvar[i], count=200).items()
+
+    #Avoid rate limit by waiting 16 minutes every time the limit is reached
+    while True:
+        try:
+            user = next(users)
+        except tweepy.TweepError:
+            time.sleep(60*16)
+            user = next(users)
+        except StopIteration:
+            break
+        user_list.append("@" + user.screen_name)   
+    accountvec = [accountvar[i]] * (len(user_list))
+    tempdf = pd.DataFrame({'User':user_list, 'Page': accountvec})
+
+    user_list_df = user_list_df.append(tempdf)
+
+    del [user_list, tempdf]
